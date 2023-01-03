f2661fd5539a9684866ec79e6708851f363fec44
Jesse Koldewijn <jesse@iungo.nl>
2023-01-03 11:13:18 +0100

    partial: Setup for tauri:command flow to ping ip-array

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,23 @@
+extern crate hyper;
+
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn getLocalIungos(iprange: Box<[i32]>) {
+    let client = Client::new();
+    let availableClients: Box<[i32]> = Box::new([]);
+    let res = client.get("http://example.domain").send().unwrap();
+
+    for current in 0..iprange.len() {
+        println!("{}", lang[current]);
+    }
 }
