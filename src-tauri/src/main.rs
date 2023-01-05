7f85de428270dc5ecb3de863d70308e5330e467d
Jesse Koldewijn <jesse@iungo.nl>
2023-01-05 10:28:07 +0100

    partial: Backend command can be called just doesnt function yet

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,24 @@
     windows_subsystem = "windows"
 )]
 
+use reqwest;
+
 fn main() {
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![fetch_devices])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[tauri::command]
+async fn fetch_devices(ip_addresses: Vec<String>) -> Result<Vec<String>, String> {
+    let mut responses = Vec::new();
+    for ip in ip_addresses {
+        let ipaddress = ip;
+        let mut url = String::from("http://");
+        url.push_str(&ipaddress);
+        let response = reqwest::get(&url).await.map_err(|e| e.to_string())?;
+        responses.push(response.text().await.map_err(|e| e.to_string())?);
+    }
+    Ok(responses)
 }
