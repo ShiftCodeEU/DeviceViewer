a357db9a76303c2db0b977044755edd7f336f1a7
Jesse Koldewijn <jesse@iungo.nl>
2023-01-06 08:38:42 +0100

    fix: Removed old broken code that throws off CI check

--- src/utils/tauri/IsTauri.ts
+++
@@ -1,9 +0,0 @@
-const handleIsTauri = () => {
-  return Boolean(
-    typeof window !== "undefined" &&
-      window !== undefined &&
-      window.__TAURI_IPC__ !== undefined
-  );
-};
-
-export default handleIsTauri;
